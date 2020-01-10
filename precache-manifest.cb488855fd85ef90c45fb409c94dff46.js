50e3f2be35fc0dd3707ddc0837f9902c0998a245
daphne Fang <daphneys.fang@gmail.com>
2020-01-09 22:35:41 -0800

    Updates

---
+++ precache-manifest.cb488855fd85ef90c45fb409c94dff46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd48ef5a769db617deededc89116b61",
+    "url": "/trelloyes/index.html"
+  },
+  {
+    "revision": "b7977375535527f94c4f",
+    "url": "/trelloyes/static/css/main.29ea8a7b.chunk.css"
+  },
+  {
+    "revision": "6863a9e25e7b97a37371",
+    "url": "/trelloyes/static/js/2.fac3bd3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/trelloyes/static/js/2.fac3bd3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7977375535527f94c4f",
+    "url": "/trelloyes/static/js/main.da2985a8.chunk.js"
+  },
+  {
+    "revision": "7c45f1a52fb7b1f774a0",
+    "url": "/trelloyes/static/js/runtime-main.2b06735e.js"
+  }
+]);
